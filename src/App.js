06dcff480391b401286b6a87c7fc583869f90dab
JonnyG8 <dotavo111@gmail.com>
2024-01-26 18:38:48 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState, useRef} from 'react';
 
 function App() {
+  const [number, setNumber] = useState(0);
+
+  let ref = useRef(number);
+  console.log(ref.current)
+
+  let refCount = () => {
+    return ref.current + 1;
+  };
+
+  console.log(refCount());
+
+  let plus = () => {
+    setNumber(number + 1);
+  }
+
+  console.log('Rerender is complete')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        onClick={plus}>
+        Click
+      </button>
+      <p>{number}</p>
     </div>
   );
 }
